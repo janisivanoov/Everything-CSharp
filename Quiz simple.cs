b27c1d6ebbacbb2a50c6b17c31154827ac4a5044
Ivan(Janis) <85873435+janisivanoov@users.noreply.github.com>
2021-07-29 12:02:32 +0300

    Simple Quiz(C#)

---
+++ Quiz simple.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Quiz_simple_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Quiz");
+            Console.Write("Choose 1,2 or 3: ");
+            string chooseNumber = Console.ReadLine();
+            if(chooseNumber == "1") {
+                string message = "You won a car!";
+                Console.WriteLine(message);
+            }
+            else if(chooseNumber == "2")
+            {
+                string message = "You won a house!";
+                Console.WriteLine(message);
+            }
+            else if(chooseNumber == "3")
+            {
+                string message = "you won cup of tea";
+                Console.WriteLine(message);
+            }
+            if(chooseNumber != "1" && chooseNumber != "2" && chooseNumber != "3")
+            {
+                string message = "Incorrect input!";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
