6c58114b9c8159b3952bbdfa82ddff1d79717b85
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-02 12:09:47 +0300

    Add files via upload

---
+++ String(all).cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace String_all_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "My\"so-called\"life";
+            Console.WriteLine(myString);
+            Console.ReadLine();
+            /*
+            string myString = "My \n life";
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "My c:\\";
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = @"My c:\";
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = String.Format("{0} = {1}","First","Second");
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = String.Format("{0:C},123.45);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = String.Format("{0:P},.123);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = String.Format("{0:(###)###-####},1234567890);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            StringBuilder myString = new StringBuilder();
+            for(int = 0;i < 100;i++){
+            myString.Append("--");
+            myString.Append(i);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "";
+            for(int i =0;i < 100;i++){
+            myString +="--"+i.ToString();
+            }
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "That russian was good";
+            myString = myString.Substring(6,14);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "That russian was good";
+            myString = myString.ToUpper();
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "That russian was good";
+            myString = myString.Replace(" ","--");
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "That russian was good";
+            myString = my.String.Remove(6,14);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+            string myString = "That russian was good";
+            myString = String.Format("Length before {0} -- Length after {1}, myString.Length , myString.Trim().Length);
+            Console.WriteLine(myString);
+            Console.ReadLine();
+            */
+        }
+    }
+}
