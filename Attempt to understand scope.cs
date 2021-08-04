fd6901f037cd714f29caa0e98301cdd1cbcddb77
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-04 11:52:01 +0300

    Add files via upload

---
+++ Attempt to understand scope.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Try_To_Understand_Scope
+{
+    class Program
+    {
+        private static string k = "";
+        static void Main(string[] args)
+        {
+            string j = "";
+            for(int i=0; i< 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Outside of the for: " + k);
+            Console.Write("Outside of the for: " + j);
+            Method();
+            Car myCar = new Car();
+            myCar.Do();
+            Console.ReadLine(); 
+        }
+        static void Method()
+        {
+            Console.WriteLine("Value of k from the method: " + k);
+        }
+    }
+    class Car
+    {
+        public void Do()
+        {
+            Console.WriteLine(Method());
+        }
+        private string Method()
+        {
+            return "Hola amigo";
+        }
+    }
+}
