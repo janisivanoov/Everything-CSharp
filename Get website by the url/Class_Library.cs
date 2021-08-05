44c4156c900578bc68677ea053c86805ea8b68bd
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-05 11:19:47 +0300

    Add files via upload

---
+++ Get website by the url/Class_Library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace ClassLibrary1
+{
+    public class Class_Library
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebPage(url);
+        }
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            string reply = GetWebPage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+        private string GetWebPage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
---
+++ Get website by the url/MyClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class_Library myClass_Library = new Class_Library();
+            string value = myClass_Library.ScrapeWebPage("https://www.unic.ac.cy/");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
