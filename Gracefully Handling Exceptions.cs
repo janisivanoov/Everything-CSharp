e165d89cf5978554ff7288b8412db87f67b6c6c8
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-10 15:20:20 +0300

    Add files via upload

---
+++ Gracefully Handling Exceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Gracefully_Handling_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = FileStyleUriParser.ReadAllText(@"C:\File.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("There is a problem with file name");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There is a problem with directory of file");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There is a problem" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Closing application");
+            }
+            Console.ReadLine();
+        }
+    }
+}
