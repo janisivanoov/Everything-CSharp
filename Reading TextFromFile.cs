6bb9d3d9c25811b31159e48e133c7b4ebf652c35
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-11 17:50:25 +0300

    Add files via upload

---
+++ Reading TextFromFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ReaderOfFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(FileStream fstream = File.OpenRead(@"D:\text.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine("Text from file" + textFromFile);
+            }
+            Console.ReadLine();
+        }
+    }
+}
