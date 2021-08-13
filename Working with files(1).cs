b6c6a1026e76bc0bdb5a6dbb2ad2f88bfa0239b3
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-13 23:35:30 +0300

    Add files via upload

---
+++ Working with files(1).cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Working_with_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello there";
+            System.IO.WriteAllText(@"C:\Example\Example.txt", text);
+            Console.ReadLine();
+        }
+    }
+}
