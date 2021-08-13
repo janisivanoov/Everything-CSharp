07b6330e5f1caa7143537cbe0e624aa3c5b60b11
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-13 23:35:18 +0300

    Add files via upload

---
+++ If statment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace If_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 3, a = 4, b = 1;
+            x = ((y + a) * (a - b));
+            if ((x > y) && (a > b))
+            {
+                Console.WriteLine("((x > y) && (a > b))");
+            }
+            if ((x > y) || (a > b))
+            {
+                Console.WriteLine("((x > y) || (a > b))");
+            }
+            else
+            {
+                Console.WriteLine("Press Enter to stop");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Programm done");
+        }
+    }
+}
