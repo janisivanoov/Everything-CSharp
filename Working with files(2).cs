d62402bab6343d564eb2e0c97693e88798d965ea
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-13 23:35:40 +0300

    Add files via upload

---
+++ Working with files(2).cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Web;
+namespace Working_with_files_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(@"https://unic.ac.cy");
+            Console.WriteLine(reply);
+            Console.ReadLine();
+        }
+    }
+}
