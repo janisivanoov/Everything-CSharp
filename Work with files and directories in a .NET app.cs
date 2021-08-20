c2c53e8e74d03b2eb5db5e32e17d9481038a2f48
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-20 20:28:04 +0300

    Add files via upload

---
+++ Work with files and directories in a .NET app.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Work_with_files_and_directories_in_a_.NET_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+
+            foreach (var dir in listOfDirectories)
+            {
+                Console.WriteLine(dir);
+            }
+            IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+            IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+            foreach (var file in allFilesInAllFolders)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
