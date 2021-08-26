ced4b76cea8157ca76462b625fbac42c9bc57d89
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-26 14:28:36 +0300

    Add files via upload

---
+++ For Statment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace For_Statment_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("We found seven");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
