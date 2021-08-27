466c42c471a622c3f5a64217ebefc9cf89df62e8
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-27 17:18:38 +0300

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee EMP = new employee();
+            EMP.name = ("Janis");
+            EMP.salary = 29999.9M;
+            Console.WriteLine("{0} name and salary is {1:C}", EMP.name, EMP.salary);
+        }
+    }
+    class employee
+    {
+        public string name
+        {
+            get;
+            set;
+        }
+        public decimal salary
+        {
+            get;
+            set;
+        }
+    }
+}
