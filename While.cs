e5e0de13cf455d8b0daba7ec485d49902e6b3f79
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-08-29 11:32:26 +0300

    Add files via upload

---
+++ While.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace While_simple_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 or 2");
+            string answer = Console.ReadLine();
+            double ans = double.Parse(answer);
+            if (ans == 1)
+            {
+                do
+                {
+                    Console.WriteLine("1");
+                } while (ans == 1);
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
