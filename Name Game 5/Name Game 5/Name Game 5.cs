2c298988f204dda9a9fd24d4c80e3bee9bd24cfb
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-01 21:02:35 +0300

    Add files via upload

---
+++ Name Game 5/Name Game 5/Name Game 5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Name_Game_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to name game 4 again");
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter a city where you born: ");
+            string city = Console.ReadLine();
+            DisplayResult(ReverseString(firstName),ReverseString(lastName),ReverseString(city));
+        }
+        private static string ReverseString(string message){
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+        private static void DisplayResult( string reversedFirstName, string reversedLastName, string reversedcity)
+        {
+            Console.Write(String.Format("{0} {1} {2}", reversedFirstName, reversedLastName, reversedcity));
+        }
+    }
+}
