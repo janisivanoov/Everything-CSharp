0d355f1712508720ef7a793a5258078a69e95ed1
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-11 13:53:27 +0300

    Add files via upload

---
+++ Time/Time And Date/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Time_And_Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
