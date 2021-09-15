0c6b42e9a84ff6686d9fca45a1581743cc51111e
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-15 18:07:24 +0300

    Add files via upload

---
+++ Exceptions/Gracefully Handling Exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gracefully_Handling_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
