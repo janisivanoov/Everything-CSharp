4a62b8172683a169b532be53fed71c4997161f6a
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-18 14:24:39 +0300

    Add files via upload

---
+++ JSON/JSON solution/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace JSON_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started program");
+            string TextFilePath = @"D:\tokens.json";
+            string textt = File.ReadAllText(TextFilePath);
+            using (StreamReader r = new StreamReader(TextFilePath))
+            {
+                string line;
+                int num = 1;
+                using (WebClient client = new WebClient())
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        client.DownloadFile(line, AppDomain.CurrentDomain.BaseDirectory + $"{num} " + ".jpg");
+                        num++;
+                    }
+            }
+            Console.WriteLine("Program done");
+            Console.ReadLine();
+        }
+    }
+}
