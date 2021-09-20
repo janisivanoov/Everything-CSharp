373a61695e5d8704e1c591fcce9f926163aba66f
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-20 08:55:57 +0300

    Add files via upload

---
+++ Scope/Try To Understand Scope/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Try_To_Understand_Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
