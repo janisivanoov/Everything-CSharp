2dd5670ea6fa1e427fbd65af5d14bbb1df818425
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-27 18:56:01 +0300

    Add files via upload

---
+++ Events/Events and Event Driven Programming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using System.Timers;
+
+namespace Events_and_Event_Driven_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Start();
+            Console.ReadLine();
+        }
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs ev)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm::ss.fff}", ev.SignalTime);
+        }
+    }
+}
