2ef912d7f186154130be99284322040d91d6678a
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-28 14:12:04 +0300

    Add files via upload

---
+++ For, do while, if, else/For, do while, if, else, foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For__do_while__if__else__foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
