8b880ad71d0b514acc1bd780718a66ca7089dbe9
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-09-29 10:12:25 +0300

    Add files via upload

---
+++ Files and directories in a .NET/Work with files and directories in a .NET app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work_with_files_and_directories_in_a_.NET_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
