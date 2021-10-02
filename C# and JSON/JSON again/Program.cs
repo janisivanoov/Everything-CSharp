bef66f64bf64768ca4011b690deaa07de0a331cb
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-02 19:23:10 +0300

    Add files via upload

---
+++ C# and JSON/JSON again/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace JSON_again
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
