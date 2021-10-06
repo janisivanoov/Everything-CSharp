ad885bb1dfcf252450608a7ac27f97608105e0a8
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-06 19:38:58 +0300

    Add files via upload

---
+++ Working_With_JSON/Working_With_JSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started program");
+            string TextFilePath = @"D:\just-urls.txt";
+            string textt = File.ReadAllText(TextFilePath);
+            using (StreamReader r = new StreamReader(TextFilePath))
+            {
+                string line;
+                int num = 0;
+                using (WebClient client = new WebClient())
+                    while ((line = r.ReadLine()) != null)  
+                    {
+                        client.DownloadFile(line, AppDomain.CurrentDomain.BaseDirectory + $"{num} " + ".jpg");
+                        num++;
+                    }
+            }
+            Console.WriteLine("Program done");
+            Console.ReadLine();
+        }
+    }
+}
