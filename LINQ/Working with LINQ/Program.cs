1858470449d12cb7d6c9c375f28fa29a56c1798c
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-08 17:47:05 +0300

    Add files via upload

---
+++ LINQ/Working with LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_with_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
