86a959d70e218b15a8f8515483ef0f0bd3330d0a
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-11 20:21:58 +0300

    Add files via upload

---
+++ JSON and uloading at S3(AWS)/Bad Solution with JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bad_Solution_with_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
