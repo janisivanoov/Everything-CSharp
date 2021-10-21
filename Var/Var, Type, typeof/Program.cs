f5e2d2c6713dbfa513e549b8d77f54214541ef0b
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2021-10-21 19:45:21 +0300

    Add files via upload

---
+++ Var/Var, Type, typeof/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Var__Type__typeof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
